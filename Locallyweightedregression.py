d3b9eb69acf56c70f636e29b48995874980e2f6b
Varshitha y s <86124941+Varshithays@users.noreply.github.com>
2024-01-03 19:03:51 +0530

    Update Locallyweightedregression.py

--- Locallyweightedregression.py
+++ Locallyweightedregression.py
@@ -1,20 +1,28 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
-import statsmodels.api as sm
 
-x= [i/5.0 for i in range(30)]
-y=[1,2,1,2,1,1,3,4,5,4,5,6,5,6,7,8,9,10,11,11,12,11,11,10,12,11,11,10,9,13]
+x = np.linspace(-5, 5, 1000)
+y = np.log(np.abs((x ** 2) - 1) + 0.5)
+x = x + np.random.normal(scale=0.05, size=1000) 
+plt.scatter(x, y, alpha=0.3)
+def local_regression(x0, x, y, tau): 
+    x0 = np.r_[1, x0]
+    x = np.c_[np.ones(len(x)), x]
+    xw =x.T * radial_kernel(x0, x, tau) 
+    beta = np.linalg.pinv(xw @ x) @ xw @ y 
+    return x0 @ beta
 
-lowess=sm.nonparametric.lowess(y,x)
-lowess_x=list(zip(*lowess))[0]
-lowess_y=list(zip(*lowess))[1]
 
-f=interp1d(lowess_x,lowess_y,bounds_error=False)
-xnew=[i/10.0 for i in range(100)]
-ynew=f(xnew)
+def radial_kernel(x0, x, tau):
+    return np.exp(np.sum((x - x0) ** 2, axis=1) / (-2 * tau ** 2))
 
-plt.plot(x,y,'o')
-plt.plot(lowess_x,lowess_y,'+')
-plt.plot(xnew,ynew,'-')
 
-plt.show()
+def plot_lr(tau):
+    domain = np.linspace(-5, 5, num=500)
+    pred = [local_regression(x0, x, y, tau) for x0 in domain] 
+    plt.scatter(x, y, alpha=0.3)
+    plt.plot(domain, pred, color="red") 
+    return plt
+
+
+plot_lr(1).show()
